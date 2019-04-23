7c986e8c402ca61ce0920377a7d2b9fcb379344c
mattfemia <mattfemia1@gmail.com>
2019-04-23 14:01:49 -0400

    finished pyds1 module

--- PyDS1/lambda.py
+++ PyDS1/lambda.py
@@ -58,3 +58,18 @@ result = reduce(lambda item1, item2: item1 + item2, stark)
 # Print the result
 print(result)
 
+
+
+
+
+##### Final Lambda fxn challenge
+
+# Select retweets from the Twitter DataFrame: result
+result = filter(lambda x: x[0:2] == 'RT', tweets_df['text'])
+
+# Create list from filter object result: res_list
+res_list = list(result)
+
+# Print all retweets in res_list
+for tweet in res_list:
+    print(tweet)
