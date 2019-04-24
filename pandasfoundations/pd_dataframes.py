eacdb51495756d4c709c336f3d2ede4eaf63da59
mattfemia <mattfemia1@gmail.com>
2019-04-24 14:37:48 -0400

    Finished data inspection and ingestion chapter for pandasfoundation. Started EDA

---
+++ pandasfoundations/pd_dataframes.py
@@ -0,0 +1,64 @@
+##### Working with pandas dataframes from memory
+
+# Import numpy
+import numpy as np
+
+# Create array of DataFrame values: np_vals
+np_vals = df.values
+
+# Create new array of base 10 logarithm values: np_vals_log10
+np_vals_log10 = np.log10(np_vals)
+
+# Create array of new DataFrame by passing df to np.log10(): df_log10
+df_log10 = np.log10(df)
+
+# Print original and new data containers
+[print(x, 'has type', type(eval(x))) for x in ['np_vals', 'np_vals_log10', 'df', 'df_log10']]
+
+
+
+
+
+##### Building pandas dataframes from scratch #####
+
+
+#1 Constructing DF from lists:
+
+# Zip the 2 lists together into one list of (key,value) tuples: zipped
+zipped = list(zip(list_keys, list_values))
+
+# Inspect the list using print()
+print(zipped)
+
+# Build a dictionary with the zipped list: data
+data = dict(zipped)
+
+# Build and inspect a DataFrame from the dictionary: df
+df = pd.DataFrame(data)
+print(df)
+
+
+
+#2 Building DF with broadcasting
+
+# Make a string with the value 'PA': state
+state = "PA"
+
+# Construct a dictionary: data
+data = {'state':state, 'city':cities}
+
+# Construct a DataFrame from dictionary data: df
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+print(df)
+
+
+
+#3 Labeling data
+
+# Build a list of labels: list_labels
+list_labels = ("year", "artist", "song", "chart weeks")
+
+# Assign the list of labels to the columns attribute: df.columns
+df.columns = list_labels
